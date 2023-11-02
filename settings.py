458c4a0faf0f514c330d11b421935e540cc473e3
Meyit47 <148498525+Meyit47@users.noreply.github.com>
2023-11-02 23:25:27 +0300

    settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "123456789876:Abkkanbsbaks"
+TOKEN = "6755023931:AAHdI4iutyPLGi28WrDL6Ua8epON4pxGWVI"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
