3eec0346f0edc650db989cb7302fbef606c08b03
MEYİTZADE <silaatesss.31@gmail.com>
2023-12-26 08:04:40 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "6755023931:AAHdI4iutyPLGi28WrDL6Ua8epON4pxGWVI"
+TOKEN = "6803669543:AAF3IVN9OQvi0DFESZSX2Nc4W2YGFBO6__c"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
